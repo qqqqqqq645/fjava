dd525218765649ed35128fc0437627b72e0e940f
dreg5 <dreg5@DESKTOP-ETUI7RU>
2019-10-05 17:11:55 +0900

    f

--- src/acmodifier/AccessEx.java
+++ src/acmodifier/AccessEx.java
@@ -8,6 +8,13 @@ public class AccessEx {
 			sample.a = 10;
 			//sample.b = 10;
 			sample.c = 10;
+			int [] ab[]=new int [3][2];
+			int [][] abb[] = new int [1][2][3];
+			System.out.println("\\n");
+			char aff='\u0041';
+			System.out.println(aff);
+			short jj = (short)'A';
+			System.out.println(jj);
 	}
 
 }
---
+++ src/testIimport/TestImport.java
@@ -0,0 +1,12 @@
+package testIimport;
+
+import java.util.Scanner;
+
+public class TestImport {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Scanner aa = new Scanner(System.in);
+	}
+
+}
