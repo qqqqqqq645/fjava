d597015fd9d51e13290ac4cbde29e3195fb6d784
dreg5 <dreg5@DESKTOP-ETUI7RU>
2019-10-05 19:37:48 +0900

    asdf

--- src/testIimport/TestImport.java
+++ src/testIimport/TestImport.java
@@ -1,12 +1,24 @@
 package testIimport;
 
-import java.util.Scanner;
-
+//import java.util.*;
 public class TestImport {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			Scanner aa = new Scanner(System.in);
+			//Scanner aa = new Scanner(System.in);
+			//int b = aa.nextInt();
+			//System.out.println(b).println();
+	
+			int i = 0;
+			String cc = "asdf";
+			System.out.println(cc);
+			System.out.println(cc.getClass());
+			System.out.println(cc.getClass().getName());
+			cc=cc+"asdf";
+			System.out.println(cc);
+			System.out.println(cc.toString());
+			Integer ii= 5;
+			System.out.println(ii.toString());
 	}
 
 }
